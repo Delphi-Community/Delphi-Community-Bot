2adc0827c08f5557ae224985296e4f5bed0bb393
dtpfl <kontakt@dtpfl.de>
2024-01-25 19:07:42 +0100

    cleanup

--- removeRSS.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'removeRss',
-  description: 'Removes an RSS URL',
-  execute(message, args) {
-      // args[0] should be the RSS URL
-      // Implement logic to remove RSS URL from the database/file
-  },
-};
